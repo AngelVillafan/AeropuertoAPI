5d4ca9a41d43f07a776c3f834219a7645e5ed65f
Angel <191G0239@rcarbonifera.tecnm.mx>
2022-12-29 16:29:23 -0600

    Add project files.

---
+++ AdministracionVuelosMovil/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AdministracionVuelosMovil
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
