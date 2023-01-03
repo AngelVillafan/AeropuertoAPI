da2b68fffe0c83769b1ceb30fb3bdb8ccbf1fc20
Miguel JH <mjhsh34@gmail.com>
2023-01-03 02:41:39 -0600

    Vista Agregar

--- AdministracionVuelosMovil/App.xaml.cs
+++ AdministracionVuelosMovil/App.xaml.cs
@@ -8,7 +8,7 @@ namespace AdministracionVuelosMovil
         {
             InitializeComponent();
 
-            MainPage = new HomeView();
+            MainPage = new AgregarView();
         }
     }
 }
---
+++ AdministracionVuelosMovil/Views/AgregarView.xaml.cs
@@ -0,0 +1,9 @@
+namespace AdministracionVuelosMovil.Views;
+
+public partial class AgregarView : ContentPage
+{
+	public AgregarView()
+	{
+		InitializeComponent();
+	}
+}
